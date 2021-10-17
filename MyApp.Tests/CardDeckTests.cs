439cb7e15a8477b445679a4dd9bb235ad0ed2f8d
Catalin-Andronie <catalin.andronie@gmail.com>
2021-10-17 15:14:15 +0300

    Add CardDeck tests

---
+++ MyApp.Tests/CardDeckTests.cs
@@ -0,0 +1,50 @@
+using System.Linq;
+using Xunit;
+
+namespace MyApp.Tests
+{
+    public sealed class CardDeckTests
+    {
+        [Fact]
+        public void CardDeck_Should_Have_52_Cards()
+        {
+            const int defaultNrOfCardsInDeck = 52;
+            var cardDeck = new CardDeck();
+            Assert.Equal(defaultNrOfCardsInDeck, cardDeck.Cards.Length);
+        }
+
+        [Fact]
+        public void CardDeck_Should_Contain_4_Suits_With_13_Cards_Each()
+        {
+            // Arrange
+            var defaultSuitOfCards = new[]
+            {
+                new Card("A", 11),
+
+                new Card("J", 10),
+                new Card("K", 10),
+                new Card("Q", 10),
+                new Card("10", 10),
+
+                new Card("2", 2),
+                new Card("3", 3),
+                new Card("4", 4),
+                new Card("5", 5),
+                new Card("6", 6),
+                new Card("7", 7),
+                new Card("8", 8),
+                new Card("9", 9)
+            };
+
+            // Act
+            var cardDeck = new CardDeck();
+
+            // Assert
+            foreach (var card in defaultSuitOfCards)
+            {
+                var nrOfSuits = cardDeck.Cards.Count(c => c.Name == card.Name && c.Value == card.Value);
+                Assert.Equal(4, nrOfSuits);
+            }
+        }
+    }
+}
