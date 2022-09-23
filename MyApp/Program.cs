cf2d3d8aabf0417b882648f47ca5226b44a48451
Catalin-Andronie <catalin.andronie@gmail.com>
2022-09-23 21:37:14 +0300

    Add PlayNewGame() method

--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -8,16 +8,7 @@ namespace MyApp
         {
             try
             {
-                var random = new Random();
-                var players = new[]
-                {
-                    new Player("Player-1", riskAversion: random.Next(4)),
-                    new Player("Player-2", riskAversion: random.Next(4)),
-                    new Player("Player-3", riskAversion: random.Next(4)),
-                    new Player("Player-4", riskAversion: random.Next(4))
-                };
-                var game = new BlackJackGame(players);
-                game.Start();
+                var game = PlayNewGame();
                 game.WriteResultTo(Console.Out);
             }
             catch (Exception e)
@@ -30,6 +21,21 @@ namespace MyApp
             {
                 Console.ResetColor();
             }
+        }
+
+        private static BlackJackGame PlayNewGame()
+        {
+            var random = new Random();
+            var players = new[]
+            {
+                    new Player("Player-1", riskAversion: random.Next(4)),
+                    new Player("Player-2", riskAversion: random.Next(4)),
+                    new Player("Player-3", riskAversion: random.Next(4)),
+                    new Player("Player-4", riskAversion: random.Next(4))
+                };
+            var game = new BlackJackGame(players);
+            game.Start();
+            return game;
         }
     }
 }
