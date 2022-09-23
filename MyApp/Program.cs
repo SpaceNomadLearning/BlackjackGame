dbee7056fc2042405e405acfcab8c230df4c7fcc
Catalin-Andronie <catalin.andronie@gmail.com>
2022-09-23 21:35:42 +0300

    Catch all exceptions in the program

--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -6,17 +6,30 @@ namespace MyApp
     {
         private static void Main()
         {
-            var random = new Random();
-            var players = new[]
+            try
             {
-                new Player("Player-1", riskAversion: random.Next(4)),
-                new Player("Player-2", riskAversion: random.Next(4)),
-                new Player("Player-3", riskAversion: random.Next(4)),
-                new Player("Player-4", riskAversion: random.Next(4))
-            };
-            var game = new BlackJackGame(players);
-            game.Start();
-            game.WriteResultTo(Console.Out);
+                var random = new Random();
+                var players = new[]
+                {
+                    new Player("Player-1", riskAversion: random.Next(4)),
+                    new Player("Player-2", riskAversion: random.Next(4)),
+                    new Player("Player-3", riskAversion: random.Next(4)),
+                    new Player("Player-4", riskAversion: random.Next(4))
+                };
+                var game = new BlackJackGame(players);
+                game.Start();
+                game.WriteResultTo(Console.Out);
+            }
+            catch (Exception e)
+            {
+                Console.ForegroundColor = ConsoleColor.DarkRed;
+                Console.WriteLine();
+                Console.WriteLine($"Program crashed with next message '{e.Message}'.");
+            }
+            finally
+            {
+                Console.ResetColor();
+            }
         }
     }
 }
